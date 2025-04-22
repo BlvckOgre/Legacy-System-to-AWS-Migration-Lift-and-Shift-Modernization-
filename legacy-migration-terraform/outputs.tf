085a02af147b0ed72ced6600f8b45da52407651c
Thatoyaka Dimakatso Malope <dimakatso.malopemojapelo@gmail.com>
2025-04-22 02:00:53 +0200

    Delete legacy-migration-terraform directory

--- legacy-migration-terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "ec2_public_ip" {
-  value = aws_instance.web_server.public_ip
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.postgres.endpoint
-}
-
--- legacy-migration-terraform/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-key_name    = "Claudia"
-db_name     = "banking"
-db_user     = "postgres"
-db_password = "MojapeloPassword123!"
-
-
-
